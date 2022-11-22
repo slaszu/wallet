412a52a32f5113034d1c76b36ba325f0f4cb08e1
Piotr Flasza <piotr.flasza@enp.pl>
2022-11-22 09:34:10 +0100

    junit + maven.surefire

---
+++ src/test/java/pl/slaszu/wallet/domain/WalletTest.java
@@ -0,0 +1,23 @@
+package pl.slaszu.wallet.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletTest {
+
+    @Test
+    @DisplayName("Add money to wallet")
+    void add() {
+        assertEquals(2,2);
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void amount() {
+    }
+}
